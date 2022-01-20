db0cc7d124e163dd4b17fa62b88e4ce32f1350f0
Bidyut-Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2022-01-20 18:40:20 +0600

    Add files via upload

---
+++ PSD to HTML/js/main.js
@@ -0,0 +1,18 @@
+
+
+// Initialize the slider
+
+  $(document).ready(function(){
+    $('.slider-area').bxSlider();
+    $('#main-menu').meanmenu({
+      meanMenuContainer:'#mobile-menu',
+      meanScreenWidth:991,
+    });
+  });
+
+
+
+
+
+
+
---
+++ PSD to HTML/js/main2.js
@@ -0,0 +1,17 @@
+
+ $(document).ready(function(){
+    $('.slider-area').bxSlider();
+    $('#main-menu').meanmenu({
+      meanMenuContainer:'#mobile-menu',
+      meanScreenWidth:768,
+    });
+  });
+
+
+console.log('bidyut sidker')
+
+
+
+
+
+
